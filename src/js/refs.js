35511cc1628ae4d58b522e81bc83ef8984b50c24
Malk0v <106482398+Malk0v@users.noreply.github.com>
2023-02-05 13:37:45 +0200

    Create refs.js

---
+++ src/js/refs.js
@@ -0,0 +1,8 @@
+export const refs = {
+  modalWindow: document.querySelector('.lightbox'),
+  body: document.querySelector('body'),
+  galleryContainer: document.querySelector('.js-gallery'),
+  imageContainer: document.querySelector('.lightbox__content'),
+  closeModalBtn: document.querySelector('.lightbox__button'),
+  deleteImg: document.querySelector('.lightbox__image'),
+};
